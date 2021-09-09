110db5bdcd09e3177319734a633b6a0a71d994dc
Aubrey Jones <aubrey_jones07@hotmail.com>
2021-09-09 00:22:05 -0600

    update links again

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
    <>
    <Router>
      <NavBar />
-  <Route path="/MealCategories" exact component={MealCategories} />
+  <Route path="/" exact component={MealCategories} />
    <Footer />
    </Router>
    </>
